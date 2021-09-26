7b74ae30e0cab99d77016401f825df9262f3e48b
Abdülkadir AKYILDIZ <abdulkadir.akyildiz@aurorabilisim.com>
2021-09-26 18:30:15 +0300

    edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/`,
+        path: `${__dirname}/src/content/data/`,
       },
     },
     `gatsby-transformer-remark`,
